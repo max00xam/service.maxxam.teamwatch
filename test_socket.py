93348254b1cfc47191a09940c71d7d65ad6eb53b
max00xam <maxxam@chammy.info>
2019-06-04 20:28:37 +0200

    Update test_socket.py

--- test_socket.py
+++ test_socket.py
@@ -1,1 +1,13 @@
+import time
+from lib.messenger import SockClient
 
+client = SockClient('user1@test.com', 'password')
+
+@client._got_message
+def new_message(data):
+    print 'on message event fired!'
+
+if __name__ == '__main__':
+    client.connect()
+    time.sleep(3)
+    client.disconnect()
